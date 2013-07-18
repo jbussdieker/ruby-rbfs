36301b0665ffb8d2d20e17b42d283d8f4b58eae1
Joshua Bussdieker <jbussdieker@gmail.com>
2013-07-18 16:35:29 -0700

    Fix color when done

--- lib/rbfs/logger.rb
+++ lib/rbfs/logger.rb
@@ -7,20 +7,20 @@ module Rbfs
     end
 
     def info(msg)
-      puts "\033[0;32m#{msg}"
+      puts "\033[0;32m#{msg}\033[0;37m"
     end
 
     def error(msg)
-      puts "\033[0;31m#{msg}"
+      puts "\033[0;31m#{msg}\033[0;37m"
     end
 
     def critical(msg)
-      puts "\033[0;31m#{msg}"
+      puts "\033[0;31m#{msg}\033[0;37m"
       exit 1
     end
 
     def notice(msg)
-      puts "\033[0;33m#{msg}"
+      puts "\033[0;33m#{msg}\033[0;37m"
     end
   end
 end
