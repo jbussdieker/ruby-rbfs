57aaede773e509a3d57b0bf33616abd9c6b6e0b7
Joshua Bussdieker <jbussdieker@gmail.com>
2013-07-18 16:56:37 -0700

    Fix color reset again

--- lib/rbfs/logger.rb
+++ lib/rbfs/logger.rb
@@ -7,20 +7,20 @@ module Rbfs
     end
 
     def info(msg)
-      puts "\033[0;32m#{msg}\033[1;37m"
+      puts "\033[0;32m#{msg}\033[00masdf"
     end
 
     def error(msg)
-      puts "\033[0;31m#{msg}\033[1;37m"
+      puts "\033[0;31m#{msg}\033[00m"
     end
 
     def critical(msg)
-      puts "\033[0;31m#{msg}\033[1;37m"
+      puts "\033[0;31m#{msg}\033[00m"
       exit 1
     end
 
     def notice(msg)
-      puts "\033[0;33m#{msg}\033[1;37m"
+      puts "\033[0;33m#{msg}\033[00m"
     end
   end
 end
